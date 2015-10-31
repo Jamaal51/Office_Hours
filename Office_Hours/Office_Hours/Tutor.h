e78e2233ddeb4807d5197e7d21c78a626db35f01
Jamaal Sedayao <you@example.com>
2015-10-31 12:31:42 -0400

    added tableview functionality

---
+++ Office_Hours/Office_Hours/Tutor.h
@@ -0,0 +1,22 @@
+//
+//  Tutor.h
+//  Office_Hours
+//
+//  Created by Jamaal Sedayao on 10/31/15.
+//  Copyright © 2015 Jamaal Sedayao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Tutor : NSObject
+
+@property (nonatomic) NSString *name;
+@property (nonatomic) NSString *imageString;
+@property (nonatomic) NSString *borough;
+@property (nonatomic) NSString *expertLanguage;
+@property (nonatomic) NSString *officeName;
+@property (nonatomic) NSString *officeHours;
+@property (nonatomic) NSString *officeLocation;
+@property (nonatomic) NSString *bio;
+
+@end
---
+++ Office_Hours/Office_Hours/Tutor.m
@@ -0,0 +1,15 @@
+//
+//  Tutor.m
+//  Office_Hours
+//
+//  Created by Jamaal Sedayao on 10/31/15.
+//  Copyright © 2015 Jamaal Sedayao. All rights reserved.
+//
+
+#import "Tutor.h"
+
+@implementation Tutor
+
+
+
+@end
